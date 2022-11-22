859f69640284d8381cbccbb1ff950049bcbc93c6
Fabricio <fabriciopolicarpo0@gmail.com>
2022-11-22 13:04:29 -0500

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,111 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
+import databaseConfig from './config/database.config';
+import authConfig from './config/auth.config';
+import appConfig from './config/app.config';
+import mailConfig from './config/mail.config';
+import fileConfig from './config/file.config';
+import facebookConfig from './config/facebook.config';
+import googleConfig from './config/google.config';
+import twitterConfig from './config/twitter.config';
+import appleConfig from './config/apple.config';
+import * as path from 'path';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthAppleModule } from './auth-apple/auth-apple.module';
+import { AuthFacebookModule } from './auth-facebook/auth-facebook.module';
+import { AuthGoogleModule } from './auth-google/auth-google.module';
+import { AuthTwitterModule } from './auth-twitter/auth-twitter.module';
+import { I18nModule } from 'nestjs-i18n/dist/i18n.module';
+import { I18nJsonParser } from 'nestjs-i18n/dist/parsers/i18n.json.parser';
+import { HeaderResolver } from 'nestjs-i18n';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { MailConfigService } from './mail/mail-config.service';
+import { ForgotModule } from './forgot/forgot.module';
+import { MailModule } from './mail/mail.module';
+import { HomeModule } from './home/home.module';
+import { WinstonModule } from 'nest-winston';
+import { transports } from 'winston';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AppResolver } from './app.resolver';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        authConfig,
+        appConfig,
+        mailConfig,
+        fileConfig,
+        facebookConfig,
+        googleConfig,
+        twitterConfig,
+        appleConfig,
+      ],
+      envFilePath: ['.env'],
+    }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: (configService: ConfigService) => {
+        return {
+          sortSchema: configService.get('graphql.sortSchema'),
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          installSubscriptionHandlers: true,
+          buildSchemaOptions: {
+            numberScalarMode: 'integer',
+          },
+          debug: configService.get('graphql.debug'),
+          playground: configService.get('graphql.playgroundEnabled'),
+          introspection: configService.get('graphql.introspection'),
+          context: ({ req, res }) => ({ req, res }),
+        };
+      },
+      inject: [ConfigService],
+    }),
+    WinstonModule.forRoot({
+      level: 'info',
+      transports: [new transports.Console()],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    MailerModule.forRootAsync({
+      useClass: MailConfigService,
+    }),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        fallbackLanguage: configService.get('app.fallbackLanguage'),
+        parserOptions: {
+          path: path.join(
+            configService.get('app.workingDirectory'),
+            'src',
+            'i18n',
+            'translations',
+          ),
+        },
+      }),
+      parser: I18nJsonParser,
+      inject: [ConfigService],
+      resolvers: [new HeaderResolver(['x-custom-lang'])],
+    }),
+    UsersModule,
+    FilesModule,
+    AuthModule,
+    AuthFacebookModule,
+    AuthGoogleModule,
+    AuthTwitterModule,
+    AuthAppleModule,
+    ForgotModule,
+    MailModule,
+    HomeModule,
+  ],
+  providers: [AppResolver],
+})
+export class AppModule {}
